8943d6e9f3f5be26849590d4d81cb4dbe8b2a81e
Dhanush-HU-07 <eng24am0163@dsu.edu.in>
2025-09-29 12:08:05 +0530

    Add files via upload

---
+++ Java/revise.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class revise{
+    public static void main(String[] a1){
+        for(int i=0; i<10;i++){
+            System.out.println(i+"The number is "+i);
+        }
+    }
+}
+   
